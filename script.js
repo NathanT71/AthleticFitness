fb28d3ac916cabe1d72a79dd0551407e8f54ddb5
NathanT71 <lchsstu188736@gmail.com>
2024-11-19 11:18:12 -0500

    JavaBG

--- script.js
+++ script.js
@@ -7,6 +7,6 @@ if (d.getHours() > 20 || d.getHours() < 8) {
     body.style["backgroundColor"] = "black";
     body.style["color"] = "white"
 } else {
-    body.style["backgroundColor"] = "blue";
+    body.style["backgroundColor"] = "#48494b";
     body.style["color"] = "black"
 }
