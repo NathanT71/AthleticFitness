86b126a7b7deb56c6c89fbf846d407731dee9fce
NathanT71 <lchsstu188736@gmail.com>
2024-11-21 22:28:52 -0500

    Toggle Button

--- script.js
+++ script.js
@@ -3,10 +3,4 @@ console.log('Hello!');
 const body = document.body
 const d = new Date();
 
-if (d.getHours() > 20 || d.getHours() < 8) {
-    body.style["backgroundColor"] = "black";
-    body.style["color"] = "white"
-} else {
-    body.style["backgroundColor"] = "grey";
-    body.style["color"] = "black"
-}
+
